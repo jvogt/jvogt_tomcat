e6b9d2fdde31dfe9329f8b31d0c32fa26602f85c
Jeff Vogt <jvogt@Jeffs-MacBook-Pro.local>
2017-09-20 22:36:30 -0700

    Update default recipe for backwards compatibility

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,4 +3,11 @@
 # Recipe:: default
 #
 
+jvogt_tomcat 'default' do
+  run_user node['jvogt_tomcat']['user']
+  run_group node['jvogt_tomcat']['group']
+  install_path node['jvogt_tomcat']['install_path']
+  java_package node['jvogt_tomcat']['java']['package_name']
+  tomcat_binary_uri node['jvogt_tomcat']['tomcat_binary']['uri']
+  tomcat_binary_checksum node['jvogt_tomcat']['tomcat_binary']['checksum']
 end
