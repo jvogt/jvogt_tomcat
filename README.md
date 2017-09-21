9d8fd543582d14a6dd53959185fc26c74b101bfd
Jeff Vogt <jvogt@Jeffs-MacBook-Pro.local>
2017-09-20 19:29:10 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# jvogt_tomcat
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: jvogt_tomcat
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
