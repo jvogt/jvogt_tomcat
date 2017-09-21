e3cd8c0e802d71db1e5dccf8e49844551413e759
Jeff Vogt <jvogt@Jeffs-MacBook-Pro.local>
2017-09-20 23:02:06 -0700

    Foodcritic updates (add issues, source urls, and LICENSE)

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,12 @@
 name 'jvogt_tomcat'
 maintainer 'Jeff Vogt'
 maintainer_email 'tomcat-cookbook-maintainer@jdccs.com'
-license 'All Rights Reserved'
+license 'Apache-2.0'
 description 'Installs/Configures jvogt_tomcat'
 long_description 'Installs/Configures jvogt_tomcat'
 version '0.2.0'
 chef_version '>= 13.0' if respond_to?(:chef_version)
-
-# issues_url 'https://github.com/jvogt/jvogt_tomcat/issues'
-# source_url 'https://github.com/jvogt/jvogt_tomcat'
+issues_url 'https://github.com/jvogt/jvogt_tomcat/issues'
+source_url 'https://github.com/jvogt/jvogt_tomcat'
 
 supports 'centos', '>= 7.0'
